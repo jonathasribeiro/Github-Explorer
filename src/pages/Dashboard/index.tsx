b689caf3a555ec53e61badc0217f12560a6bf548
jonathasribeiro151 <jonathasribeiroreal@gmail.com>
2020-09-15 12:40:15 -0300

    feature: inicial project and global style

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { Container, Title } from './styles';
+
+const Dashboard: React.FC = () => {
+  return <Title>Explore repositórios no Github</Title>;
+};
+
+export default Dashboard;
