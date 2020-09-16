e30d3ea277ecd73e13751680aee370f7502aedf9
jonathasribeiro151 <jonathasribeiroreal@gmail.com>
2020-09-16 17:08:47 -0300

    feature: prototype repository info page

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
-import { useRouteMatch } from 'react-router-dom';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+import { Container, Header, RepositoryInfo, Issues } from './styles';
 
-// import { Container } from './styles';
+import logo from '../../assets/logo.svg';
 
 interface RepositoryParams {
   repository: string;
@@ -10,7 +12,55 @@ interface RepositoryParams {
 const Repository: React.FC = () => {
   const { params } = useRouteMatch<RepositoryParams>();
 
-  return <h1>Repository:{params.repository}</h1>;
+  return (
+    <Container>
+      <Header>
+        <img src={logo} alt="Github Explorer" />
+        <Link to="/">
+          <FiChevronLeft size={16} />
+          Voltar
+        </Link>
+      </Header>
+
+      <RepositoryInfo>
+        <header>
+          <img
+            src="https://avatars0.githubusercontent.com/u/28929274?v=4"
+            alt="Rocketseat"
+          />
+          <div>
+            <strong>Rocketseat Unform</strong>
+            <p>Decricao do repo</p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>808</strong>
+            <span>Stars</span>
+          </li>
+          <li>
+            <strong>48</strong>
+            <span>Forks</span>
+          </li>
+          <li>
+            <strong>67</strong>
+            <span>Issues abertas</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+
+      <Issues>
+        <Link to="/">
+          <div>
+            <strong>repository.full_name</strong>
+            <p>repository.description</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
+    </Container>
+  );
 };
 
 export default Repository;
